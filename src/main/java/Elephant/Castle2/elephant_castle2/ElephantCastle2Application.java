e543e7cf82e1a57df2cc10d7e3e8d126a2ce413d
Bernice Oppong <b22boateng@gmail.com>
2021-06-25 19:46:37 -0400

    all files

---
+++ src/main/java/Elephant/Castle2/elephant_castle2/ElephantCastle2Application.java
@@ -0,0 +1,13 @@
+package Elephant.Castle2.elephant_castle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantCastle2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElephantCastle2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Elephant/Castle2/elephant_castle2/ElephantCastle2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Elephant.Castle2.elephant_castle2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElephantCastle2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
